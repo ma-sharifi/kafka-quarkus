f28edcb912973256aa554e15c619988d5fe16ba3
Mahdi Sharifi <m.sharifi@bluerocktms.com>
2023-04-28 14:12:05 +0200

    first commit

---
+++ src/main/java/org/acme/ReactiveMessagingExample.java
@@ -0,0 +1,42 @@
+package org.acme;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.smallrye.mutiny.Multi;
+import jakarta.enterprise.context.ApplicationScoped;
+
+//generate --> [ source-a ] --> process --> [processed-a] --> filter --> [ sink-a ] --> consume
+@ApplicationScoped
+public class ReactiveMessagingExample
+{
+  //generate --> [ source-a ]
+  @Outgoing("source-a")
+  public Multi<String> source() {
+    return Multi.createFrom().items("hello", "from", "SmallRye", "reactive", "messaging");
+  }
+
+ //[ source-a ] --> process --> [ processed-a ]
+  @Incoming("source-a")
+  @Outgoing("processed-a")
+  public Message<String> process(Message<String> in) {
+    System.out.println("#Process the payload. #Read->manipulate->Write");
+    String payload = in.getPayload().toUpperCase();
+    // Create a new message from `in` and just update the payload
+    return in.withPayload(payload);
+  }
+
+  //[ processed-a ] --> process --> [ sink-a]
+  @Incoming("processed-a")
+  @Outgoing("sink-a")
+  public Multi<String> filter(Multi<String> input) {
+    return input.select().where(item -> item.length() > 4);
+  }
+  //[ sink:processed-b ] --> consume
+  @Incoming("sink-a")
+  public void sink(String word) {
+    System.out.println(">> " + word);
+  }
+
+}
